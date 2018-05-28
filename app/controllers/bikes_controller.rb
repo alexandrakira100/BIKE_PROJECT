d9bcc17a77d5bc8f7af548f8d845f68ab5c57a24
Hiroaki <hiroaki.fukuoka@gmail.com>
2018-05-28 19:06:30 +0900

    added #index #ner #create action to bike controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,10 +1,20 @@
 class BikesController < ApplicationController
   def index
+    @bikes = Bike.all
   end
 
   def new
+    bike = Bike.new
   end
 
   def create
+    bike = Bike.new()
   end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:)
+  end
+
 end

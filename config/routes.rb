be981bfa5421e04bb791fab8ef7069a7d2ff006e
Hiroaki <hiroaki.fukuoka@gmail.com>
2018-05-29 13:44:59 +0900

    nested renral #create inside bikes routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :bikes
-
-  resources :rentals, only: [:index, :create, :destroy]
+  resources :bikes, only: [:index, :show, :new, :create] do
+    resources :rentals, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  end
+  resources :rentals, only: [:index, :destroy]
 end

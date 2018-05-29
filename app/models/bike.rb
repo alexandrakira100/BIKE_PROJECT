25429c53ee7e37131464cdd1082ab8fddf3f5362
Hiroaki <hiroaki.fukuoka@gmail.com>
2018-05-29 18:18:09 +0900

    fixed size enum problem and created create new render

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   validates :description, presence: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
   enum size: [:small, :medium, :large]
   validates :size, presence: true
   validates :price_per_day, presence: true

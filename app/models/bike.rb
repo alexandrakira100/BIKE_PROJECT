04ab2c96d9a7183e5e02bf63a6c6448284ad3def
Ammelie Diemont <Ammelie.diemont@gmail.com>
2018-05-31 14:51:44 +0900

    worked on add new bikes

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -5,4 +5,7 @@ class Bike < ApplicationRecord
   enum size: [:small, :medium, :large]
   validates :size, presence: true
   validates :price_per_day, presence: true
+
+  NEIGHBOURHOODS = ["Shibuya", "Shinjuku", "Ginza", "Omotesando", "Asakusa", "Harajuku", "Meguro"]
+  PRICE_CHOICES = [500, 1000, 1500, 2000, 2500, 3000, 3500]
 end

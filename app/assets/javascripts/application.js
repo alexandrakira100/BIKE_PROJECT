887eaf006fb6cfabb0f1058273b49e65c219bed7
Tim Cheah <tim.cheah.89@gmail.com>
2018-05-31 12:49:55 +0900

    Made all rented bikes have an unavailable status in the Index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
 //= require rails-ujs
 //= require_tree .
+const unavailableBikes = document.querySelectorAll(".unavailable-bike");
+console.log(unavailableBikes)
+unavailableBikes.forEach((bike) => {
+  console.log(bike.parentNode)
+  bike.parentNode.addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+});

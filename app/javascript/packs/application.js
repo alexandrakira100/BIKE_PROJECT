f3367890b64b5d1f32f1e9232148377f37c84954
Tim Cheah <tim.cheah.89@gmail.com>
2018-06-01 11:59:37 +0900

    Pimped up the index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+const unavailableBikes = document.querySelectorAll(".unavailable-bike");
+console.log(unavailableBikes)
+unavailableBikes.forEach((bike) => {
+  console.log(bike.parentNode)
+  bike.parentNode.addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+});

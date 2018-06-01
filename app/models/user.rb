cae8f368c543a0346caabb4276afdcf2b6bfbc52
Alexandra Doll <alexandra.doll100@gmail.com>
2018-06-01 12:45:24 +0900

    my_bikes_controller, view and design fnished

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :password, presence: true
+  has_many :bikes
+
 end
---
+++ test/controllers/owner_bikes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OwnerBikesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
